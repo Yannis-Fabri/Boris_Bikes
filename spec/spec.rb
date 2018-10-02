8effe82b91d00b2510e097ad340aa381e24ccf31
EC2 Default User <ec2-user@ip-172-31-7-171.us-east-2.compute.internal>
2018-10-02 15:58:55 +0000

    bike class

--- spec/spec.rb
+++ spec/spec.rb
@@ -4,6 +4,11 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 end
 
+describe Working do
+  it { is_expected.to return : }
+end
+
+
 describe Bike do
   it { is_expected.to respond_to :working? }
 end 
