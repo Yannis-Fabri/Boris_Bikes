a73bcc88425687ad74442de27866ce5bcc5b89e3
EC2 Default User <ec2-user@ip-172-31-7-171.us-east-2.compute.internal>
2018-10-02 15:35:35 +0000

    spec

--- spec/spec.rb
+++ spec/spec.rb
@@ -1,7 +1,5 @@
 require './day1.rb'
 
-Describe DockingStation do
-    it 'release_bike' do
-        expect(subject).to respond_to(:release_bike)
-    end
+describe DockingStation do
+  it { is_expected.to respond_to :release_bike }
 end
