2a18d6126200500282451a19e5acf7d4194d8a6f
Yannis Fabri <yannis.fabri@cgi.com>
2018-10-04 15:09:09 +0000

    Day3

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,19 +5,25 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   
   it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
     bike = subject.release_bike
-    expect(bike).to be_working
+    expect(bike.working?).to eq true
   end
+  
+  it { is_expected.to respond_to(:dock).with(1).argument }
+  
+  it { is_expected.to respond_to(:bike) }
   
   it 'It can dock a bike' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq bike
   end
   
-  it 'return a docked bike' do
+  it 'returns docked bike' do
     bike = Bike.new
-    subject.(bike)
-    expect(subject.release_bike).to eq bike
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
   
   describe '#release_bike' do
